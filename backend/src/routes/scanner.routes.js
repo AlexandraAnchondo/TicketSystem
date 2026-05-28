4a1e9eeabe0bdd1501a8f478921481f2cae47760
AlexandraAnchondo <alexandra.anchondo.17@gmail.com>
2026-05-28 08:18:43 -0700

    Remove scanner component

--- backend/src/routes/scanner.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const scannerController = require("../controllers/scanner.controller");
-
-router.post("/scanner/start", scannerController.startScannerService);
-router.post("/scanner/stop", scannerController.stopScannerService);
-
-module.exports = router;
